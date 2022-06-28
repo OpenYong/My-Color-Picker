086577a283229435973ab5b9e311a7eb2f654f16
Yong <fkszm3@gmail.com>
2022-06-28 22:36:42 +0900

    created reacte app, added palette

---
+++ src/components/Palette.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className="palette">
+      <div className="palette-colors"></div>
+    </div>
+  );
+};
+
+export default Palette;
