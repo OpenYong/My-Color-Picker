9788aaad65403544fbf300ac626c8efd69bb6214
Yong <fkszm3@gmail.com>
2022-07-09 23:07:52 +0900

    Add adding,removing ColorPalette feature

--- src/components/Palette/DraggableColorContainer.js
+++ src/components/Palette/DraggableColorContainer.js
@@ -2,15 +2,27 @@ import React from "react";
 import styles from "./DraggableColorContainer.module.css";
 
 const DraggableColorContainer = (props) => {
-  const { onClick, color } = props;
+  const { onColorPick, dataIndex, color, onAddColor, onRemoveColor } = props;
   return (
-    <div
-      className={styles["color-container"]}
-      onClick={() => onClick(color)}
-      style={{ backgroundColor: color }}
-    >
+    <div className={styles["color-container"]}>
+      <div
+        className={styles["color-background"]}
+        onClick={() => onColorPick(dataIndex)}
+        style={{ backgroundColor: color }}
+      ></div>
       <div className={styles["color-container__right"]}>
-        <div className={styles["add-btn"]}> +</div>
+        <div
+          className={styles["add-btn"]}
+          onClick={() => onAddColor(dataIndex)}
+        >
+          +
+        </div>
+        <div
+          className={styles["add-btn"]}
+          onClick={() => onRemoveColor(dataIndex)}
+        >
+          -
+        </div>
       </div>
     </div>
   );
--- src/components/Palette/NewPaletteForm.js
+++ src/components/Palette/NewPaletteForm.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./NewPaletteForm.module.css";
 import { HexColorPicker } from "react-colorful";
 import DraggableColorContainer from "./DraggableColorContainer";
+import chroma from "chroma-js";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const NewPaletteForm = () => {
   const [colors, setColors] = useState([
@@ -24,37 +29,100 @@ const NewPaletteForm = () => {
   ]);
   const [isPickerOpen, setIsPickerOpen] = useState(false);
   const [clickedColorBox, setClickedColorBox] = useState();
+  const paletteNameInput = useRef();
 
-  const colorContainerClickHandler = (clickedColor) => {
-    const clickedColorIndex = colors.findIndex(
-      (colorObject) => colorObject.color === clickedColor
-    );
-
-    setClickedColorBox(clickedColorIndex);
+  const colorContainerClickHandler = (dataIndex) => {
+    setClickedColorBox(dataIndex);
     setIsPickerOpen((prevState) => !prevState);
   };
-  const pickerHandelr = (color, clickedColorBox) => {
-    let newColorArry = [...colors];
-    newColorArry[clickedColorBox].color = color;
-    setColors(newColorArry);
+
+  const addColorClickHandler = (dataIndex) => {
+    let targetScaleColor;
+    if (colors[dataIndex + 1] === undefined) {
+      targetScaleColor = "#000000";
+    } else {
+      targetScaleColor = colors[dataIndex + 1].color;
+    }
+
+    let newColorArray = [...colors];
+    newColorArray.splice(dataIndex + 1, 0, {
+      name: "New Color",
+      color: chroma
+        .scale([colors[dataIndex].color, targetScaleColor])
+        .mode("lab")
+        .colors(1)[0],
+    });
+    setColors(newColorArray);
+  };
+
+  const removeColorHandler = (dataIndex) => {
+    if (colors.length > 1) {
+      let newColorArray = [...colors];
+      newColorArray.splice(dataIndex, 1);
+      setColors(newColorArray);
+    } else {
+      return;
+    }
+  };
+
+  const pickerHandler = (color, clickedColorBox) => {
+    let newColorArray = [...colors];
+    newColorArray[clickedColorBox].color = color;
+    setColors(newColorArray);
+  };
+
+  const savePaletteHandler = (e) => {
+    e.preventDefault();
+    const newPalette = {
+      paletteName: paletteNameInput.current.value,
+      id: paletteNameInput.current.value,
+      emoji: "🎨",
+      colors: colors,
+    };
+    console.log(newPalette);
+
+    paletteNameInput.current.value = "";
   };
 
   return (
     <div className={styles["main-container"]}>
       <div className={styles["new-palette"]}>
-        {colors.map((color) => (
+        {colors.map((color, index) => (
           <DraggableColorContainer
-            onClick={colorContainerClickHandler}
+            key={Math.random().toString()}
+            dataIndex={index}
+            onColorPick={colorContainerClickHandler}
             color={color.color}
+            onAddColor={addColorClickHandler}
+            onRemoveColor={removeColorHandler}
           />
         ))}
       </div>
       {isPickerOpen && (
         <HexColorPicker
           color={colors[clickedColorBox].color}
-          onChange={(color) => pickerHandelr(color, clickedColorBox)}
+          onChange={(color) => pickerHandler(color, clickedColorBox)}
         />
       )}
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => savePaletteHandler(e)}
+      >
+        <TextField
+          id="standard-basic"
+          label="팔레트 명"
+          variant="standard"
+          inputRef={paletteNameInput}
+        />
+        <Button variant="outlined" type="submit">
+          만들기
+        </Button>
+      </Box>
     </div>
   );
 };
